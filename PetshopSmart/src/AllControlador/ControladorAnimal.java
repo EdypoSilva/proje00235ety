fe678f692f0411c120451a4e93ef5513edacb5e2
EdypoSilva <fellype_150@hotmail.com>
2016-06-09 18:42:05 -0300

    Controlador 1.1
    Ajustando regras do programa

--- PetshopSmart/src/AllControlador/ControladorAnimal.java
+++ PetshopSmart/src/AllControlador/ControladorAnimal.java
@@ -7,6 +7,7 @@ package AllControlador;
 
 import AllClass.Animal;
 import AllClass.Repositorio.RepositorioAnimal;
+import javax.swing.JOptionPane;
 
 public class ControladorAnimal {
 
@@ -71,7 +72,11 @@ public class ControladorAnimal {
         String testDados = validarCamposAnimal(cadAnimal);
         if (testDados.equals("1")) {
             testDados = "1";
-            //RepositorioAnimal.setCadastroAnimal(cadAnimal);
+            /*if(RepositorioAnimal.setCadastroAnimal(cadAnimal)){
+                JOptionPane.showMessageDialog(null,"Cadastrado Com Sucesso!!!");
+            }else{
+                JOptionPane.showMessageDialog(null, "Não Cadastrado!!!");
+            }*/
         }
         return testDados;
     }
@@ -97,17 +102,34 @@ public class ControladorAnimal {
 
     public static String alterarAnimal(Animal cadAnimal, int posi) {
         String testDados = validarCamposAnimal(cadAnimal);
-        if (testDados.equals("1")){  
-            //RepositorioAnimal.setAlterarAnimal(Animal cadAnimal, int posi);
+        if (testDados.equals("1") && (posi >= 0)) {
+            int resp = JOptionPane.showConfirmDialog(null, "Deseja Remover o Animal?");
+            if (resp == 0) {
+            /*if (RepositorioAnimal.setAlterarAnimal(Animal cadAnimal, int posi)) {
+                JOptionPane.showMessageDialog(null,"Alterado Com Sucesso!!!");
+            }else{
+                 JOptionPane.showMessageDialog(null, "Não Alterado!!!");
+                   }*/
+            }
         }
         
         return testDados;
     }
 
-    
-    
-    
-    
-    
-    
+    public static String deletarAnimal(String cpf, int posi) {
+        String erros = "";
+        if (!cpf.equals("") && (posi >= 0)) {
+            int resp = JOptionPane.showConfirmDialog(null, "Deseja Remover o Animal?");
+            if (resp == 0) {
+                /*if(RepositorioAnimal.setDeletarAnimal(cpf, posi)){
+                JOptionPane.showMessageDialog(null,"Removido Com Sucesso!!!");
+            }else{
+                JOptionPane.showMessageDialog(null,"Erro, Não Removido!!!");
+            }*/
+            }
+        }
+        
+        return erros;
+    }
+
 }
